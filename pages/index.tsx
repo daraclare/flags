38b1b366cfdd5a54ed23db851c9dfce932a1e621
Dara Carolan <daraclare@gmail.com>
2020-10-11 15:04:47 +0100

    styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,92 +2,66 @@ import React from "react";
 import Head from "next/head";
 import Nav from "../components/nav";
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+const Home = ({ data }) => {
+  console.log("data", data);
 
-    <Nav />
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-    <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
+      <Nav />
 
-      <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
+      <div className="title">
+        <h1>Welcome to the Flag App!</h1>
+        <p>Find a country below and click to find their bordering countries</p>
       </div>
-    </div>
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-);
+      <main>
+        {data.map((country) => {
+          return (
+            <div className="card">
+              <img width="50px" src={country.flag} alt="" />
+              <h2>{country.name}</h2>
+              <p>NativeName: {country.nativeName}</p>
+              <p>Capital: {country.capital}</p>
+            </div>
+          );
+        })}
+      </main>
+
+      <style jsx>{`
+        main {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        }
+        .title {
+          width: 100%;
+          color: #333;
+          text-align: center;
+        }
+
+        .card {
+          padding: 18px 18px 24px;
+          width: 300px;
+          border: 1px solid #9b9b9b;
+          margin: 5px;
+        }
+        .card :hover {
+          background: lightgrey;
+        }
+      `}</style>
+    </div>
+  );
+};
 
 export async function getStaticProps() {
   const res = await fetch("https://restcountries.eu/rest/v2/all");
   const data = await res.json();
+
   return {
     props: {
       data,
